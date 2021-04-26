4284b0db5a54f322b5bee210751b123c3a131a33
smartwalle <smartwalle@gmail.com>
2021-04-26 15:13:08 +0800

    session

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/smartwalle/dbm"
+	"time"
 )
 
 type User struct {
@@ -13,7 +14,8 @@ type User struct {
 }
 
 func main() {
-	var cfg = dbm.NewConfig("mongodb+srv://smartwalle:smartwalle@smartwalle.kbxxd.mongodb.net/?retryWrites=true&w=majority")
+	//var cfg = dbm.NewConfig("mongodb+srv://smartwalle:smartwalle@smartwalle.kbxxd.mongodb.net/?retryWrites=true&w=majority")
+	var cfg = dbm.NewConfig("mongodb://192.168.1.77:27017")
 
 	var client, err = dbm.NewClient(context.Background(), cfg)
 	if err != nil {
@@ -25,7 +27,20 @@ func main() {
 	var db = client.Database("db")
 	var tUser = db.Collection("user")
 
-	var uList []*User
-	tUser.Find(context.Background(), dbm.M{}).All(&uList)
-	fmt.Println(uList)
+	//var u = &User{}
+	//u.Id = dbm.NewObjectId()
+	//tUser.InsertOne(context.Background(), u)
+
+	for {
+		var uList []*User
+		tUser.Find(context.Background(), dbm.M{}).Limit(1).All(&uList)
+		fmt.Println(time.Now(), uList)
+
+		//var u = &User{}
+		//u.Id = dbm.NewObjectId()
+		//tUser.InsertOne(context.Background(), u)
+
+		time.Sleep(time.Second * 2)
+	}
+
 }
