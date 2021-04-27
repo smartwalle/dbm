fafe269e7706c3bf4a7e3d5642a356a1cc176681
smartwalle <smartwalle@gmail.com>
2021-04-27 16:07:46 +0800

    完善 Find

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/smartwalle/dbm"
-	"time"
 )
 
 type User struct {
@@ -13,14 +12,9 @@ type User struct {
 	Age  int          `bson:"age"`
 }
 
-type Item struct {
-	Id   dbm.ObjectId `bson:"_id"`
-	Name string       `bson:"name"`
-}
-
 func main() {
-	var cfg = dbm.NewConfig("mongodb+srv://smartwalle:smartwalle@smartwalle.kbxxd.mongodb.net/?retryWrites=true&w=majority")
-	//var cfg = dbm.NewConfig("mongodb://192.168.1.77:30000")
+	//var cfg = dbm.NewConfig("mongodb+srv://smartwalle:smartwalle@smartwalle.kbxxd.mongodb.net/?retryWrites=true&w=majority")
+	var cfg = dbm.NewConfig("mongodb://192.168.1.77:30000")
 
 	var client, err = dbm.NewClient(context.Background(), cfg)
 	if err != nil {
@@ -29,31 +23,12 @@ func main() {
 	}
 	defer client.Close(context.Background())
 
-	var db = client.Database("db")
+	var db = client.Database("test")
 	var tUser = db.Collection("user")
 
-	var u = &User{}
-	u.Id = dbm.MustObjectId("6087a3d22364dbb6cb13892f")
-	u.Name = "xxx"
-	u.Age = 10
-
 	for i := 0; i < 100000; i++ {
-
 		var u = &User{}
 		u.Id = dbm.NewObjectId()
 		tUser.InsertOne(context.Background(), u)
 	}
-
-	for {
-		var uList []*User
-		tUser.Find(context.Background(), dbm.M{}).Limit(1).All(&uList)
-		fmt.Println(time.Now(), uList)
-
-		//var u = &User{}
-		//u.Id = dbm.NewObjectId()
-		//tUser.InsertOne(context.Background(), u)
-
-		time.Sleep(time.Second * 2)
-	}
-
 }
