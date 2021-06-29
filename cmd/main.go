e3ff1f2c8179b1f39aa6a732cc2be7e1b92ba05f
SmartWalle <smartwalle@gmail.com>
2021-06-29 21:30:18 +0800

    update demo

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/smartwalle/dbm"
-	"github.com/smartwalle/xid"
-	"strconv"
 )
 
 type User struct {
@@ -31,21 +29,49 @@ func main() {
 	var db = client.Database("test")
 	var tUser = db.Collection("user")
 
-	tUser.Drop(context.Background())
+	//tUser.Drop(context.Background())
+	//
+	//var indexView = tUser.IndexView()
+	//indexView.Drop(context.Background(), "us")
+	//indexView.CreateIndex(context.Background(), "us", []string{"server_id", "user_id"})
+	//
+	//for i := 0; i < 100; i++ {
+	//	var u = &User{}
+	//	u.ServerId = i % 10
+	//	u.UserId = xid.Next()
+	//	u.Name = "u" + strconv.Itoa(i)
+	//	u.Age = i + 1
+	//	u.Gender = i%2 + 1
+	//	u.Point = (i + 1) * 10
+	//
+	//	tUser.InsertOne(context.Background(), u)
+	//}
 
-	var indexView = tUser.IndexView()
-	indexView.Drop(context.Background(), "us")
-	indexView.CreateIndex(context.Background(), "us", []string{"server_id", "user_id"})
+	//var filter = []dbm.M{{"$match": dbm.M{"server_id": 1}}, {"$sample": dbm.M{"size": 1}}}
 
-	for i := 0; i < 100; i++ {
-		var u = &User{}
-		u.ServerId = i % 10
-		u.UserId = xid.Next()
-		u.Name = "u" + strconv.Itoa(i)
-		u.Age = i + 1
-		u.Gender = i%2 + 1
-		u.Point = (i + 1) * 10
+	var match = dbm.D{
+		{"server_id", 1},
+	}
+	var sample = dbm.D{
+		{"size", 1},
+	}
 
-		tUser.InsertOne(context.Background(), u)
+	var pipe = []dbm.D{
+		{
+			{"$match", match},
+		},
+		{
+			{"$sample", sample},
+		},
+	}
+
+	var results []*User
+	err = tUser.Aggregate(context.Background(), pipe).All(&results)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, u := range results {
+		fmt.Println(u.Id, u.ServerId, u.UserId, u.Name, u.Gender, u.Age, u.Point)
 	}
 }
